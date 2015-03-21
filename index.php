929e3fb2772a1e6f68f69e06d4451c35a25f0e6a
Shane Cavanaugh <shane@shanecav.net>
2015-03-21 00:06:47 -0400

    Add links. Duh.

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php"; ?>
 		<div id="colorful"></div>
 	<h2>Elevating Creativity that  Makes a Difference</h2>
 	<div id="social">
-		<div id="twitter"><img src="/img/twitter.png"></div>
-		<div id="facebook"><img src="/img/facebook.jpg"></div>
+		<div id="twitter"><a href="http://twitter.com/fawnworldwide" target="_blank"><img src="/img/twitter.png"></a></div>
+		<div id="facebook"><a href="http://facebook.com/fawnworldwide" target="_blank"><img src="/img/facebook.jpg"></a></div>
 		<div id="email"><a href="mailto:info@fawnworldwide.org">info@fawnworldwide.org</a></div>
 	</div>
 </header>
