aabb1a98b45cec658ee2d2802bfd0862fa5e0634
Shane Cavanaugh <shane@shanecav.net>
2015-03-22 01:55:22 -0400

    Prelimary header.
    See: http://www.fawnworldwide.org
    
    TO DO:
    - get feedback
    - crunch background image
    - roll own dropdowns
    - 'who support' alignment

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@
 		
 		<title>Forger: A Worldwide Network</title>
 
-		<link href='http://fonts.googleapis.com/css?family=Raleway:400,700,200' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="/css/youtotallyrock.css" media="screen">
+		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="/css/fawn.css" media="screen">
 
 		<?php if (isset($styles)) { echo "<style>$styles</style>"; } ?>
 
--- index.php
+++ index.php
@@ -4,14 +4,36 @@ $styles = "";
 include $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php"; ?>
 
 <header>
-	<h1>Forger: A Worldwide Network</h1>
-		<div id="colorful"></div>
-	<h2>Elevating Creativity that  Makes a Difference</h2>
-	<div id="social">
-		<div id="twitter"><a href="http://twitter.com/fawnworldwide" target="_blank"><img src="/img/twitter.png"></a></div>
-		<div id="facebook"><a href="http://facebook.com/fawnworldwide" target="_blank"><img src="/img/facebook.jpg"></a></div>
-		<div id="email"><a href="mailto:info@fawnworldwide.org">info@fawnworldwide.org</a></div>
+
+<div id="top-bar">
+	<div id="heart"><a href="/">Forger</a></div>
+	<div id="actions">
+		<button id="sign-in-button">Sign In</button>
+		<button id="join-us-button">Join Us</button>
 	</div>
+</div>
+
+<div id="headline">
+	<h1>A Worldwide Network</h1>
+	<h2>Elevating Creativity That Makes A Difference</h2>
+</div>
+
+<div id="search">
+	<h3>I&rsquo;m searching for a...</h3>
+	<div id="selection">
+		<select>
+			<option value="" class="option-selected" disabled selected>Occupation</option>
+		</select>
+		<p>who<br> supports</p>
+		<select>
+			<option value="" class="option-selected" disabled selected>Cause</option>
+		</select>
+		<p>in</p>
+		<select>
+			<option value="" class="option-selected" disabled selected>Location</option>
+		</select>
+	</div>
+</div>
 </header>
 
 <?php include $_SERVER["DOCUMENT_ROOT"] . "/includes/footer.php"; ?>
