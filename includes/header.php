e5ab8e03875ae5c8ac9af17f7ded6aa7f6feecbf
Shane Cavanaugh <shane@shanecav.net>
2015-03-22 20:56:34 -0400

    Initial mobile support.

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,10 @@
 		<title>Forger: A Worldwide Network</title>
         <meta name="description" content="Elevating Creativity That Makes A Difference">
 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href='http://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="/css/fawn.css" media="screen">
+		<link rel="stylesheet" href="/css/mobile.css" media="only screen and (max-device-width: 568px)">
 
 		<?php if (isset($styles)) { echo "<style>$styles</style>"; } ?>
 
