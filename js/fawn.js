bdb6c439ea547ae76f3ffb82e1ea8d29b591ffb0
Shane Cavanaugh <shane@shanecav.net>
2015-03-27 16:35:56 -0400

    Initial commit for search. Prelimary search submission.

--- js/fawn.js
+++ js/fawn.js
@@ -14,3 +14,17 @@ $(window).scroll(function() {
 
 //Drop-downs
 $('.drop-down').hover(function() { $('ul', $(this)).slideToggle('fast'); })
+$('.drop-down li').click(function() {
+	$(this).parent().prev().text($(this).text());
+
+	//Auto-submit search with selected parameters
+	if ( $('#search-occupation h5').text() != 'Occupation' && $('#search-cause h5').text() != 'Cause' && $('#search-location h5').text() != 'Location') {
+		
+		$occupation = $('#search-occupation h5').text();
+		$cause =  $('#search-cause h5').text();
+		$location =  $('#search-location h5').text();
+		$('<form action="/forge/" method="POST">' + 
+    		'<input type="hidden" name="occupation" value="' + $occupation + '">' +'<input type="hidden" name="cause" value="' + $cause + '">' +'<input type="hidden" name="location" value="' + $location + '">' +
+    		'</form>').submit();
+	}
+})
