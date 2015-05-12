0a65cebf92ea6f82ac52f84c26cca3e101e346bd
Shane Cavanaugh <shane@shanecav.net>
2015-05-12 10:49:42 -0400

    Show profile mobile

--- profile/show.php
+++ profile/show.php
@@ -1,25 +1,29 @@
 <?php 
 
 include_once $_SERVER["DOCUMENT_ROOT"] . "app/controller/access.controller.php";
-include_once $_SERVER["DOCUMENT_ROOT"] . "/app/model/profile.model.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "app/model/profile.model.php";
 
-$profile = new Profile();
+	$profile = new Profile;
 
-$forger = intval($_GET["forger"]);
+	$id = intval($_GET["forger"]);
 
-$username = $profile->gimme("name", "id", $forger);
-$userlocation = $profile->gimme("location", "id", $forger);
-$userwebsite = $profile->gimme("website", "id", $forger);
-$useroccupation = $profile->gimme("occupation", "id", $forger);
-$userabout = $profile->gimme("about", "id", $forger);
-$usersummary = $profile->gimme("summary", "id", $forger);
-$usercurrentprojects = $profile->gimme("currentprojects", "id", $forger);
-$userphoto = "/app/controller/avatar.controller.php?id=" . $forger;
+	$name = $profile->gimme("name", "id", $id);
+	$location = $profile->gimme("location", "id", $id);
+	$cause = $profile->gimme("cause", "id", $id);
+	$website = $profile->gimme("website", "id", $id);
+	$occupation = $profile->gimme("occupation", "id", $id);
+	$about = $profile->gimme("about", "id", $id);
+	$summary = $profile->gimme("summary", "id", $id);
+	$currentprojects = $profile->gimme("currentprojects", "id", $id);
 
-$styles = "
+	include_once $_SERVER["DOCUMENT_ROOT"] . "app/model/avatar.model.php";
+	$avatar = new Avatar;
+	$photo = "/app/controller/avatar.controller.php?id=" . $id;
+
+$styles = <<<CSS
 
 #profile {
-	width: 60%; min-width: 715px;
+	width: 60%; min-width: 960px;
 	margin: auto;
 	position: relative;
 	overflow: visible;
@@ -47,10 +51,7 @@ $styles = "
 }
 
 #profile-photo {
-	width: 225px;
-	height: 225px;
 	background-color: transparent;
-	border-radius: 50%;
 	text-align: center;
 	display: inline-block;
 	float: left;
@@ -58,7 +59,7 @@ $styles = "
 }
 
 #profile-photo img {
-	width: 225px;
+	max-width: 225px;
 	border-radius: 50%;
 }
 
@@ -151,83 +152,159 @@ h3 {
 	clear: both;
 }
 
-";
+@media only screen and (max-width: 840px) {
+
+	#profile {
+		width: 90%;
+		min-width: 0;
+		padding-top: 20px;
+	}
+
+	#profile-edit {
+		position: relative;
+		width: 100%;
+		text-align: center;
+		right: 0;
+		margin: 20px 0;
+}
+
+	#profile-name {
+		margin-bottom: 30px;
+}
+
+	#profile-name h2 {
+		font-size: 1.2em;
+		line-height: 1em;
+		font-weight: 700;
+		letter-spacing: 5px;
+		text-transform: uppercase;
+		text-align: center;
+		display: block;
+		margin: 0;
+	}
+
+	#profile-photo {
+		width: 100%;
+		display: block;
+		float: none;
+		height: auto;
+		margin-bottom: 20px;
+	}
+
+	#profile-photo h3 {
+		margin: 0;
+}
+
+	#profile-photo img {
+		width: 150px;
+	}
+
+	#info {
+		display: block;
+		width: 100%;
+		margin: 20px 0;
+		text-align: center;
+	}
+
+	#info h3 {
+		margin-bottom: 0;
+	}
+
+	#profile-occupation {
+		font-size: .8em;
+	}
+
+	#profile-location {
+		margin-bottom: 20px;
+		font-size: .8em;
+	}
+
+	#profile-website {
+		font-size: .6em;
+}
+
+	#profile-summary {
+		margin-top: 0;
+		font-size: .8em;
+	}
+
+	#profile-summary, #profile-about, #profile-currentprojects {
+		margin: 30px 0;
+	}
+
+	textarea {
+		margin: 0;
+	}
+
+	h3 {
+		line-height: 1.4em;
+		letter-spacing: 5px;
+	}
+}
+
+CSS;
 include_once $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php";
 ?>
 
+<div id="status"></div>
+
 <div id="profile">
 
-	<?php 
-	if ( $forger == $id ) { ?>
-		<div id="profile-edit">
-			&#x270e; <button type="button" id="edit">Edit Your Profile</button>
-		</div>
+<div id="profile-photo">
+	<?php if(!empty($photo)) { ?>
+		<h3><img src="<?php echo $photo; ?>"></h3>
 	<?php } ?>
-
-	<div id="profile-photo">
-		<?php if(!empty($userphoto)) { ?>
-			<h3><?php echo "<img src=\"" . $userphoto . "\">"; ?></h3>
-		<?php } else { ?>
-			<?php } ?>
+</div>
+<div id="info">
+	<div id="profile-name">
+		<?php if(!empty($name)) { ?>
+			<h2><?php echo $name ?></h2>
+		<?php } ?>
 	</div>
 
-	<div id="info">
-			<div id="profile-name">
-				<?php if(!empty($username)) { ?>
-					<h2 class="saved"><?php echo $username ?></h2>
-				<?php } else { ?>
-				<?php } ?>
-			</div>
-
-			<div id="profile-occupation">
-				<?php if(!empty($useroccupation)) { ?>
-					<h3 class="saved"><?php echo $useroccupation ?></h3>
-				<?php } else { ?>
-				<?php } ?>
-			</div>
-
-			<div id="profile-location">
-				<?php if(!empty($userlocation)) { ?>
-					<h3 class="saved"><?php echo $userlocation ?></h3>
-				<?php } else { ?>
-				<?php } ?>
-			</div>
-
-			<div id="profile-website">
-				<?php if(!empty($userwebsite)) { ?>
-					<h3><a href="http://<?php echo $userwebsite ?>" target="_blank"><?php echo $userwebsite ?></a></h3>
-				<?php } ?>
-			</div>
-		</div>
-
-		<div style="clear: both;"></div>
-
-		<div id="profile-summary">
-			<?php if(!empty($usersummary)) { ?>
-				<div><?php echo $usersummary; ?></div>
-			<?php } else { ?>
-			<?php } ?>
-		</div>
-
-		<div id="profile-about">
-			<?php if(!empty($userabout)) { ?>
-				<h3 class="saved">About</h3>
-				<div class="saved"><?php echo $userabout; ?></div>
-			<?php } else { ?>
-			<?php } ?>
-		</div>
-
-		<div id="profile-currentprojects">
-			<?php if(!empty($usercurrentprojects)) { ?>
-				<h3 class="saved">Current Projects</h3>
-				<div class="saved"><?php echo $usercurrentprojects ?></div>
-			<?php } else { ?>
-			<?php } ?>
-		</div>
-
+	<div id="profile-occupation">
+		<?php if(!empty($occupation)) { ?>
+			<h3><?php echo $occupation ?></h3>
+		<?php } ?>
 	</div>
+
+	<div id="profile-location">
+		<?php if(!empty($location)) { ?>
+			<h3><?php echo $location ?></h3>
+		<?php } ?>
+	</div>
+
+	<div id="profile-website">
+		<?php if(!empty($website)) { ?>
+			<h3><a href="http://<?php echo $website ?>" target="_blank"><?php echo $website ?></a></h3>
+		<?php } ?>
+	</div>
+</div>
+
+<div style="clear: both;"></div>
+
+<div id="profile-summary">
+	<?php if(!empty($summary)) { ?>
+		<div><?php echo $summary ?></div>
+	<?php } ?>
+</div>
+
+<div id="profile-about">
+	<?php if(!empty($about)) { ?>
+		<h3>About</h3>
+		<div><?php echo nl2br($about); ?></div>
+	<?php } ?>
+</div>
+
+<div id="profile-currentprojects">
+	<?php if(!empty($currentprojects)) { ?>
+		<h3>Current Projects</h3>
+		<div><?php echo nl2br($currentprojects); ?></div>
+	<?php } ?>
+</div>
 
 </div>
 
+</div>
 
 <?php include $_SERVER["DOCUMENT_ROOT"] . "/includes/footer.php"; ?>
