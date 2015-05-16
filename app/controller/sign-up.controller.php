f6b9e8860297bdfa3151c5021956e02e06eb2672
Shane Cavanaugh <shane@shanecav.net>
2015-05-16 13:35:51 -0400

    We no longer require an email confirmation before the user can edit their profile

--- app/controller/sign-up.controller.php
+++ app/controller/sign-up.controller.php
@@ -18,14 +18,23 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
 	}
 
 	else { 
-		$signUp->confirmEmail($email, $hash); 
+
+		// Send the user an email to confirm their email address
+		$signUp->mailConfirmation($email);
+
+		// Go ahead and get them signed-up and set their identifying number
+		$_SESSION["id"] = $signUp->insertArtist($email, $hash);
+
+		// Let them know to check for the confirmation email
 		$_SESSION["status"] = "Great! You&rsquo;re all signed&ndash;up. We&rsquo;ve sent you an email with a link to click so that we can confirm your email address. <i>Be sure and double check your spam folder.</i> :)"; 
-		header("Location: /join-us/");
+
+		// Kick them to edit their profile
+		header("Location: /profile/edit/");
 	}
 }
 
 else if ( isset($_GET["confirm"])) {
-	$email = $signUp->activateUser($_GET["confirm"]);
+	$email = $signUp->confirmEmail($_GET["confirm"]);
 	if ( $email === null ) {
 		$_SESSION["status"] = "We received an unknown confirmation key somehow.";
 		header("Location: /join-us/");
@@ -35,8 +44,8 @@ else if ( isset($_GET["confirm"])) {
 		include_once $_SERVER["DOCUMENT_ROOT"] . "/app/model/profile.model.php";
 		$profile = new Profile();
 		$_SESSION["id"] = $profile->gimme("id", "email", $email);
-		$_SESSION["status"] = "Great! You&rsquo;re all set. Please fill out your profile so people can find you and learn more about you.";
-		header("Location: /profile/edit/");
+		$_SESSION["status"] = "Thanks for confirming your email.";
+		header("Location: /profile/");
 	}
 }
 
