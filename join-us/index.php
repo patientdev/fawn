ece116d4912147ed6ef4a066336a17b584e68b0b
Shane Cavanaugh <shane@shanecav.net>
2015-05-16 15:41:59 -0400

    Remove facebook for now

--- join-us/index.php
+++ join-us/index.php
@@ -5,6 +5,14 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/app/controller/sign-up.controller.php
 
 $styles = <<<CSS
 
+h2 {
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  padding-bottom: 20px;
+  border-bottom: 2px solid #ccc;
+}
+
 form {
 	width: 40%;
 	margin: 40px auto;
@@ -119,82 +127,10 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/includes/header.php";
 
 ?>
 
-<script>
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-  
-  // This is called with the results from from FB.getLoginStatus().
-  function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      // Logged into your app and Facebook.
-     // logIn();
-    } else if (response.status === 'not_authorized') {
-      // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    } else {
-      // The person is not logged into Facebook, so we're not sure if
-      // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
-    }
-  }
-
-  window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1560529120863841',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.2' // use version 2.2
-  });
-
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-
-  };
-
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function logIn() {
-    FB.api('/me', function(response) {
-      $.post("/app/controller/sign-in.controller.php", { facebook: 'true', response: response }, function( success ) {
-        console.log(success);
-      });
-    });
-  }
-</script>
-
 <form method="post" id="sign-up" action="/app/controller/sign-up.controller.php">
 
-<button type="button" id="facebook-login">Sign Up with Facebook</button>
-<div>Or with your email:</div>
+<h2>Sign Up</h2>
+
 <input type="email" id="email" name="email" placeholder="Email address">
 <input type="password" id="password" name="password" placeholder="Password">
 <input type="password" id="confirm-password" name="confirm-password" placeholder="Confirm your password">
@@ -217,36 +153,6 @@ $('#join-us-submit').click(function(e) {
 
   else $('#sign-up').submit();
 })
-
-  $('#facebook-login').click(function(e) {
-    e.preventDefault();
-    FB.login(function(response){
-      if ( response.status == 'connected') {
-        FB.api('/me?fields=id,name,email,picture', function(user) {
-          $.post('/app/controller/facebook.controller.php', { user: user }, function(success) {
-          console.log(success);
-          window.location.replace('/profile/');
-        } );
-        })
-      }
-
-      else if ( response.status == 'not_authorized') {
-        FB.api('/me?fields=id,name,email,picture', function(user) {
-          $.post('/app/controller/facebook.controller.php', { user: user }, function(success) {
-          console.log(success);
-          window.location.replace('/profile/edit/');
-        } );
-        })
-      }
-    });
-  })
-
-$('#facebook-logout').click(function() {
-  FB.logout(function(response) {
-    console.log(response);
-  });
-})
-
 
 </script>
 DOC;
