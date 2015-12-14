2ef82c3e9dcf7aae6a19a8a80955ebf8b56ae303
Shane Cavanaugh <shane@patientdev.com>
2015-12-13 21:31:21 -0600

    Add db-connect.php template

---
+++ app/model/db-connect.php
@@ -0,0 +1,28 @@
+<!-- <?php
+
+// Template for db-connect.php
+// To be located in $DOCUMENT_ROOT/../protected/
+
+// class Database {
+//
+//     function connect() {
+//         try {
+//             $db = new PDO("mysql:host=localhost;dbname=forgers", 'root', 'igltt4t5');
+//             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//             $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+//             return $db;
+//         } catch (PDOException $e) {
+//              echo "<br>Connection error " . $e->getMessage();
+//         }
+//     }
+//
+//     public function disconnect()    {   }
+//     public function select()        {
+//
+//     }
+//     public function insert()        {   }
+//
+//
+// }
+
+?> -->
